bd5a1b124877341aad2c65bbf6509b7be71a0777
NadeemKhanFh <124039561+NadeemKhanFh@users.noreply.github.com>
2024-05-13 23:05:32 +0500

    NetzenHomes

--- src/screens/onboarding/slides.ts
+++ src/screens/onboarding/slides.ts
@@ -6,20 +6,20 @@ export const Slides = [
     slide: icons['slide-1'],
     heading: 'Find the mostexciting startup jobs.',
     content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+      'Dive into a world of innovation, connecting with exciting startup opportunities to shape the future.',
   },
   {
     id: 2,
     slide: icons['slide-2'],
     heading: 'Leading the way of recruitment.',
     content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+      'Lead the way in hiring with advanced strategies, attracting top talent and staying competitive.',
   },
   {
     id: 3,
     slide: icons['slide-3'],
     heading: 'People matter for all companies.',
     content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+      'Cultivate thriving cultures that fuel growth and prioritize employee well-being.',
   },
 ];
